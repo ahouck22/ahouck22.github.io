a2b0ba35b04011b90aa9f734825c8d9671302d74
ahouck22 <112977513+ahouck22@users.noreply.github.com>
2022-10-06 12:55:04 -0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,27 @@
+var input1 = document.getElementById("input1")
+var input2 = document.getElementById("input2")
+var input3 = document.getElementById("input3")
+var input4 = document.getElementById("input4")
+ 
+var a = Number(input1.value)
+var b = Number(input2.value)
+var c = Number(input3.value)
+var d = Number(input4.value)
+ 
+var btn = document.getElementById("btn1")
+btn.addEventListener('click', myFunction);
+ 
+function myFunction (a,b,c,d){
+    var a = Number(input1.value)
+    var b = Number(input2.value)
+    var c = Number(input3.value)
+    var d = Number(input4.value)
+ 
+    var e;
+    e = (a * c) + (b * d)
+
+    var wage = document.getElementById ("wage")
+    wage.innerHTML = e
+ 
+ 
+}
